7320991dba0790fa08002c83747cb9dc38473bf2
kishanhirani <kishanhirani79@gmail.com>
2025-01-24 00:06:30 +0530

    mongoUpdate

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const rootRouter = require('./routes/index');
 const app = express();
 app.use(express.json());
 
-mongoose.connect('mongodb+srv://devkishanhirani:Omen1234@nemeton.hmyoo.mongodb.net/')
+mongoose.connect('mongodb+srv://kishanhirani79:DEv123431@cluster0.ip2jaux.mongodb.net/')
     .then(() => console.log('Connected to MongoDB'))
     .catch((err) => console.error('Error connecting to MongoDB:', err));
 
