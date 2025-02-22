9288d606ad2354f5ed2484362f2f35b65c26d225
kishanhirani <kishanhirani79@gmail.com>
2025-02-22 20:00:30 +0530

    asd

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const rootRouter = require('./routes/index');
 
 const app = express();
 app.use(express.json());
-const isLive = 0
+const isLive = 1
 const MongoUrl = isLive ? 'mongodb+srv://kishanhirani79:Omen1234@cluster0.ip2jaux.mongodb.net/test?retryWrites=true' : "mongodb://localhost:27017/"
 mongoose.connect("mongodb://localhost:27017/")
     .then(() => console.log('Connected to MongoDB'))
